4615d149decb5212eef839b1d643018ac38b4739
Greg Lyons <gregory.lyons512@gmail.com>
2021-10-16 11:33:20 -0500

    Completed stat modifying moves data.

--- src/scraping/statModifyingMoves.py
+++ src/scraping/statModifyingMoves.py
@@ -1,43 +1,78 @@
 import csv
+from os import error
 import re
 from bs4.element import Tag
 from utils import openLink, getBulbapediaDataPath, parseName, genSymbolToNumber
 
-# EXCEPTIONS: Secret Power
-# Only Crunch, Acid, and Diamond Storm have changed their stat modifications between generations
+
+# EXCEPTIONS: ['secret_power', 'crunch', 'diamond_storm', 'acid', 'psychic', 'amnesia', 'shadow_down', 'focus_energy']
+# Secret Power has a complicated Bulbapedia description to parse, with tables
+# Crunch, Acid, Focus Energy, and Diamond Storm have changed their stat modifications between generations
 # Psychic and Amnesia have also changed their stat modifications if you count the Special split
+# Shadow Down doesn't exist
+
 
 
 # given an <a> element for a move category, parse the text to determine the stat modification and recipient of the modification, and find all moves therein
-def handleCategoryLink(link):
+def handleCategoryLink(link, writer):
   categoryLink = 'https://bulbapedia.bulbagarden.net' + link['href']
   linkText =  link.get_text()
 
   # 'Special' refers to Gen 1, when Special Attack and Special Defense were combined
-  recipient, stat = re.search(r'Moves that can (raise|lower) the (target|user)\'s (accuracy|evasion|Attack|Defense|Special Attack|Special Defense|Special|Speed)', linkText).group(2, 3)
+  print(linkText)
+  sign, recipient, stat = re.search(r'Moves that can (raise|lower) the (target|user)\'s (accuracy|evasion|Attack|Defense|Special Attack|Special Defense|Special|Speed)', linkText).group(1, 2, 3)
+  if sign == 'raise':
+    sign = '+'
+  else:
+    sign = '-'
   
   # now need to find specific move and modification
   bs = openLink(categoryLink, 0, 10)
 
   # each category is divided into groups, based on the first letter of the move name
-  groupList = bs.find(id='mw-pages').find_all('div', {'class': 'mw-category-group'})
+  moveLists = bs.find(id='mw-pages').find_all('ul')
 
-  for group in groupList:
-    moveList = group.find_next('ul').find_all('li')
-    for move in moveList:
-      moveLink = move.find('a')
-      moveName, gen, modifier = handleMoveLink(moveLink, recipient, stat)
-  return
+  print(sign, recipient, stat)
+
+  try:
+    for moveList in moveLists:
+      for move in moveList.find_all('li'):
+        moveLink = move.find('a')
+        moveName, gen, modifier = handleMoveLink(moveLink, stat)
+        if modifier != 'exception':
+          if stat == 'evasiveness':
+            stat = 'evasion'
+
+          writer.writerow([moveName, gen, parseName(stat), modifier, sign, recipient])
+        else: 
+          continue
+
+  except Exception as e:
+    print(e)
+    print('something went wrong with', sign, recipient, stat)
+    print()
+    return []
 
 # given an <a> element for a move, 
-def handleMoveLink(link, recipient, stat):
+def handleMoveLink(link, stat):
   moveLink = 'https://bulbapedia.bulbagarden.net' + link['href']
   moveName = parseName(link.get_text().removesuffix('(move)'))
 
   # handle these moves separately, due to either their complicated nature (Secret Power) or their change between generations (the rest)
-  if moveName in ['secret_power', 'crunch', 'diamond_storm', 'acid', 'psychic', 'amnesia', 'kings_shield', 'memento']:
+  if moveName in ['secret_power', 'crunch', 'diamond_storm', 'acid', 'psychic', 'amnesia', 'shadow_down', 'shadow_mist']:
     return moveName, 'exception', 'exception'
-  
+  # moves with descriptions not covered by the regex defined below
+  elif moveName in ['mist_ball', 'luster_purge']:
+    return moveName, 3, 1
+  elif moveName in ['dragon_ascent', 'hyperspace_fury']:
+    return moveName, 6, 1
+  elif moveName == 'thunderous_kick':
+    return moveName, 8, 1
+  elif moveName == 'acupressure':
+    return moveName, 4, 2
+  elif moveName == 'belly_drum':
+    return moveName, 2, 'max'
+
   bs = openLink(moveLink, 0, 10)
   descriptionStart = bs.find(id='Effect').parent
 
@@ -54,56 +89,76 @@ def handleMoveLink(link, recipient, stat):
           moveDescriptions.append(genDescription)
           genDescription = []
 
-        startGen = genSymbolToNumber(re.search(r'Generation[s]* ([IVX]*)', nextNode.get_text()).group(1).strip())
+        try:
+          startGen = genSymbolToNumber(re.search(r'Generation[s]* ([IVX]*)', nextNode.get_text()).group(1).strip())
+
+        except AttributeError:
+          print('something went wrong with', moveName)
+
         genDescription.append(startGen)
+
       elif nextNode.name == 'p':
         # get text for move in current generation
         genDescription.append(nextNode.get_text().rstrip('\n'))
+
     nextNode = nextNode.nextSibling
   moveDescriptions.append(genDescription)
 
   # if the move hasn't been altered since its introduction, then the generation will not be in the 'Effect' description. We get when it was introduced from the first <p> element
   if not isinstance(moveDescriptions[0][0], int):
-    genIntroduced = genSymbolToNumber(re.search(r'introduced in Generation ([IVX]*)\.', bs.find('h1').find_next('p').get_text()).group(1))
-    moveDescriptions[0] = [genIntroduced] + moveDescriptions[0] 
+    try:
+      genIntroduced = genSymbolToNumber(re.search(r'introduced in Generation ([IVX]*)\.', bs.find('h1').find_next('p').get_text()).group(1))
+      moveDescriptions[0] = [genIntroduced] + moveDescriptions[0] 
+
+    # indicates gen not given in description
+    except AttributeError:
+      print('something went wrong finding gen of', moveName)
 
   # figure out how many stages by which the stat is modified
   for description in moveDescriptions:
     gen = description[0]
     paragraphs = description[1:]
+
     for paragraph in paragraphs:
-      findStage_regex1 = rf"the ({recipient}|foe)'s {stat}[ stat rises]* by (one|two) stage"
-      findStage_regex2 = rf"the {stat}[ stat]* of the ({recipient}|foe) by (one|two) stage"
-      findStage1 = re.search(findStage_regex1, paragraph)
-      findStage2 = re.search(findStage_regex2, paragraph)
-      if findStage1 != None:
-        return moveName, gen, findStage1.group(2)
-      elif findStage2 != None: 
-        return moveName, gen, findStage2.group(2)
-      else:
-        findStage_regex3 = rf"the {stat}[ stat]* of all hit targets by (one|two) stage"
-        findStage_regex4 = rf"the {stat}[ stat]* of all adjacent opponents by (one|two) stage"
-        findStage_regex3 = re.search(findStage_regex3, paragraph)
-        findStage_regex4 = re.search(findStage_regex4, paragraph)
-        if findStage_regex3 != None:
-          return moveName, gen, findStage_regex3.group(1)
-        elif findStage_regex4 != None:
-          return moveName, gen, findStage_regex4.group(1)
+      findStage_regex = rf"{stat}[\w\s,()-é]* (one|two|1|2|three) [stat ]*(stage|level)"
+      other_regex = rf"evasiveness."
+
+      findStage = re.search(findStage_regex, paragraph) 
+
+      if findStage:
+        if len(findStage.group(1)) > 1:
+          if findStage.group(1) == 'one':
+            modifier = 1
+          elif findStage.group(1) == 'two':
+            modifier = 2
+          elif findStage.group(1) == 'three':
+            modifier = 3
         else:
-          print(moveName)
-          print(paragraph)
-          return moveName, gen, None
+          modifier = findStage.group(1)
+        
+        return moveName, gen, modifier
+      elif re.search(other_regex, paragraph):
+        return moveName, gen, 1
+
+  # shouldn't be reached unless Bulbapedia changes its descriptions
+  print('couldn\'t handle', moveName)
 
   return 
 
 def statModifyingMoves(fname):
   with open(fname, 'w', newline='', encoding='utf-8') as csvFile:
     writer = csv.writer(csvFile)
+    writer.writerow(['Move Name', 'Gen', 'Stat Name', 'Modifier', 'Recipient'])
+
     bs = openLink('https://bulbapedia.bulbagarden.net/wiki/Category:Moves_by_stat_modification', 0, 10)
 
     categoryList = bs.find('div', {'class': 'mw-category'}).find('ul').find_all('li')
     for categoryLink in categoryList:
-      handleCategoryLink(categoryLink.find('a'))
+      handleCategoryLink(categoryLink.find('a'), writer)
+    
+    # critical hit rate
+    for moveName in ['z_foresight', 'z_sleep_talk', 'z_tailwind', 'z_acupressure', 'z_heart_swap']:
+      writer.writerow([moveName, 7, 'critical_hit_ratio', 2, '+', 'user'])
 
   return
 
