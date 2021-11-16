32c23bf0f23797bc03ad58b9f42371d75b9ca6b7
Greg Lyons <gregory.lyons512@gmail.com>
2021-11-16 15:10:46 -0600

    Testing .gitignore.

--- src/db/index.js
+++ src/db/index.js
@@ -1,3 +1,14 @@
+require('dotenv').config();
+const mysql = require('mysql2');
 const sqlStatements = require('./sql/index.js');
 
-console.log(sqlStatements.createVersionGroupJunctionTables);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+});
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected!');
+})
